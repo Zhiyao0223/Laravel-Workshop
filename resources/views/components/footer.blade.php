1615bcfab2cb82af6e33e82b34bddd62ef5dee4e
Zhi Yao <zhiyao0223@gmail.com>
2024-06-13 17:34:34 +0800

    Fix typo

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer mt-10 py-3  bg-light">
     <div class="container text-center">
-        <span class="text-muted">Created by Zhi Yao</span>
+        <span class="text-muted">Modified by Zhi Yao</span>
     </div>
 </footer>
