2ee93a5ae8c751b9dd94f22905d76dc10bf4f1ef
Ng Zhi Yao <75945731+Zhiyao0223@users.noreply.github.com>
2024-06-24 15:56:24 +0800

    Add files via upload

---
+++ gdsc_workshop.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 24, 2024 at 09:55 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gdsc_workshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+-- Error reading structure for table gdsc_workshop.customers: #1932 - Table &#039;gdsc_workshop.customers&#039; doesn&#039;t exist in engine
+-- Error reading data for table gdsc_workshop.customers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gdsc_workshop`.`customers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table gdsc_workshop.failed_jobs: #1932 - Table &#039;gdsc_workshop.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table gdsc_workshop.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gdsc_workshop`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table gdsc_workshop.migrations: #1932 - Table &#039;gdsc_workshop.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table gdsc_workshop.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gdsc_workshop`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+-- Error reading structure for table gdsc_workshop.password_resets: #1932 - Table &#039;gdsc_workshop.password_resets&#039; doesn&#039;t exist in engine
+-- Error reading data for table gdsc_workshop.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gdsc_workshop`.`password_resets`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+-- Error reading structure for table gdsc_workshop.password_reset_tokens: #1932 - Table &#039;gdsc_workshop.password_reset_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table gdsc_workshop.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gdsc_workshop`.`password_reset_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table gdsc_workshop.personal_access_tokens: #1932 - Table &#039;gdsc_workshop.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table gdsc_workshop.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gdsc_workshop`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table gdsc_workshop.users: #1932 - Table &#039;gdsc_workshop.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table gdsc_workshop.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `gdsc_workshop`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
